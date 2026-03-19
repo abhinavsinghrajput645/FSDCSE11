be8e8d22483409b81080dc2facfb1f4185577c01
Abhinav <abhinavsinghrajpt645@gmail.com>
2026-03-19 16:17:17 +0530

    advance js

--- ADVANCE JS/pattern.js
+++ ADVANCE JS/pattern.js
@@ -1,8 +1,8 @@
-// let c=/^\d+$/;
-// console.log(c.test("123456"));
-// let reges= /(cat|dog)/;
-// console.log(reges.test("i have a dog"));
-// let regex = /hello /i;
-// console.log(regex.exec("hello world"));
+ let c=/^\d+$/;
+ console.log(c.test("123456"));
+ let reges= /(cat|dog)/;
+ console.log(reges.test("i have a dog"));
+ let regex = /hello /i;
+ console.log(regex.exec("hello world"));
 let text1="abc,bcd,cde";
 console.log(text1.split(/,/));
